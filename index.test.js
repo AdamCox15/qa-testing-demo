993cc91b71b38ba0e48502ed72bacafeaec73eaa
Adam Cox <adamcox.dev@gmail.com>
2022-09-19 13:18:37 -0600

    Final commit after demo

--- index.test.js
+++ index.test.js
@@ -15,3 +15,39 @@ let testData = {
     floor: 5,
     roomNumber: 3
   }
+
+
+  test('Test should run and 2 should equal 2', () => {
+    expect(2).toBe(2);
+  })
+ describe('testData tests', () => {
+ 
+  test('The first name of the test data should be Patten', () => {
+    expect(testData.firstName).toBe('Patten');
+  })
+  test('Last name should be Goforth', () => {
+    expect(testData.lastName).toBe('Goforth')
+  })
+
+ })
+
+ describe('formatTitle tests', () => {
+  test('formatTitle should return a string', () => {
+    let formattedTitle = formatTitle(testData.title);
+    expect(typeof formattedTitle).toBe('string')
+  })
+  test('formatTitle function should format title correctly', () => {
+    let formattedTitle = formatTitle(testData.title);
+    expect(formattedTitle).toBe('Nulla Ac')
+  })
+ })
+
+ describe('shortenBio tests', () => {
+
+  test('should return a shortened version of bio', () => {
+    let shortBio = shortenBio(testData.bio)
+    expect(shortBio.length).toBeLessThan(testData.bio.length)
+  })
+
+})
+  
